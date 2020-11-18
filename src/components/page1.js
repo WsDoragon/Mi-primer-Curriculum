0da15d7c9aa74bc5b6ffce202921f795d67ccec3
WsDoragon <berna2022@gmail.com>
2020-11-18 16:03:13 -0300

    Creada pagina principal
    index creado como pagina principal, agregado boton para ir a la creacion del curriculum

---
+++ src/components/page1.js
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/core";
+
+export const page1 = () => {
+  return (
+    <>
+      <Box padding="5px" alignSelf="center">
+        Pagina Composicion
+      </Box>
+    </>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,51 @@
-import { Heading, Stack } from "@chakra-ui/core";
+import {
+  Heading,
+  Stack,
+  Text,
+  Box,
+  Center,
+  Grid,
+  GridItem,
+  Button,
+  Link,
+} from "@chakra-ui/core";
+//import { Center, Square, Circle } from "@chakra-ui/react";
 
 import { ProbandoJavascript } from "../components/ProbandoJavascript";
 import { ProbandoAPI } from "../components/ProbandoAPI";
 import { ProbandoListas } from "../components/ProbandoListas";
 import { ProbandoInput } from "../components/ProbandoInput";
+import { page1 } from "../components/page1";
 
 export default function IndexPage() {
   return (
-    <Stack spacing="45px" padding="10px">
-      <Heading>Hello World</Heading>
-      <ProbandoJavascript />
-      <ProbandoAPI />
-      <ProbandoListas />
-      <ProbandoInput />
-    </Stack>
+    <>
+      <Stack spacing="45px" padding="10px">
+        <Heading>Mi primer Curriculum</Heading>
+        <Center>
+          <Box
+            p="6"
+            m="4"
+            flexBasis="45%"
+            borderWidth="2px"
+            borderColor="black"
+            rounded="lg"
+          >
+            Esta es una pagina web creada para apoyar a personas que requieren
+            de ayuda para crear su Curriculum para postular a trabajos
+          </Box>
+        </Center>
+        <Center>
+          <Link href="https://nextjs.org/docs">
+            <Button> Quiero comenzar mi CV</Button>
+          </Link>
+        </Center>
+      </Stack>
+    </>
   );
 }
+
+//<ProbandoJavascript />
+//<ProbandoAPI />
+//<ProbandoListas />
+//<ProbandoInput />
