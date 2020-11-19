4da43a1a2513297db8352ac86d1cd678cd459e2f
Bladimir Pardo <bladimirpardomolina@gmail.com>
2020-11-19 00:37:04 -0300

    Inicio de la pagina formulario

---
+++ src/pages/formulario.js
@@ -0,0 +1,24 @@
+import {
+  Box,
+  Center,
+  Stack,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Button,
+  Link
+} from "@chakra-ui/core";
+import {GiClick} from "react-icons/Gi"
+import { ProbandoInput } from "../components/ProbandoInput";
+
+const formulario = () => {
+  return (
+    <>
+      <ProbandoInput />
+    </>
+  );
+};
+
+export default formulario;
