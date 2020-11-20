b67b60a177ef3003d93c60a398d8879e0837f57b
PabloSz <pablosaez1995@gmail.com>
2020-11-20 15:21:12 -0300

    actualizar info para acceder a db

--- src/db.js
+++ src/db.js
@@ -2,11 +2,19 @@ import { MongoClient } from "mongodb";
 
 const uri = "mongodb://mongo.pablosz.tech?retryWrites=true&w=majority";
 
+if (!process.env.MONGODB_PASSWORD) {
+  setInterval(() => {
+    console.error(
+      "CREA UN ARCHIVO .env CON LA CONTRASEÑA PARA LA BASE DE DATOS!"
+    );
+  }, 1000);
+}
+
 const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   auth: {
-    user: "user-info104-playground",
+    user: "user-info104-grupo3",
     password:
       process.env.MONGODB_PASSWORD ||
       "Agrega MONGODB_PASSWORD=tu_contraseña en archivo .env",
@@ -16,7 +24,7 @@ const client = new MongoClient(uri, {
 export const dbConnection = client
   .connect()
   .then((client) => {
-    return client.db("info104-playground");
+    return client.db("info104-grupo3");
   })
   .catch((err) => {
     console.error(err);
